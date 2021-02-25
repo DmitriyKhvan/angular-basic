5000781eb2239119977a7c55f89af538868e2f03
freedom <dimon.twin@gmail.com>
2021-02-25 16:58:33 +0000

    params, headers

--- src/app/todos.servece.ts
+++ src/app/todos.servece.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { delay } from 'rxjs/operators'
+import { Observable, throwError } from "rxjs";
+import { catchError, delay } from 'rxjs/operators'
 
 export interface Todo {
   completed: boolean
@@ -14,12 +14,30 @@ export class TodosService {
   constructor(private http: HttpClient) {}
 
   addTodo(todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
+    const headers = new HttpHeaders({
+      'MyCustomHeader': Math.random().toString()
+    })
+
+    return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo, {
+      headers
+    })
   }
 
   fetchTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todoss?_limit=2')
-      .pipe(delay(500))
+    let params = new HttpParams
+    params = params.append('_limit', '4')
+    params = params.append('custom', 'anything')
+
+    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos', {
+      params
+    })
+      .pipe(
+        delay(500),
+        catchError(error => {
+          console.log('Error ', error.message)
+          return throwError(error)
+        })
+      )
   }
 
   removeTodo(id: number): Observable<void> {
